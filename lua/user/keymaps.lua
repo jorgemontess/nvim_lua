25285e4796301c6c9b26749a3d4fa2d1befc1ff9
Jorge <jomontes97@hotmail.com>
2022-07-12 19:17:20 -0500

    Basic Configuration keymaps and options

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,71 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap "space" as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--    normal_mode = "n",
+--    insert_mode = "i",
+--    visual_mode = "v",
+--    visual_block_mode "x",
+--    term_mode = "t",
+--    command_mode = "c""
+
+
+-- NORMAL --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<Cr>", opts)
+keymap("n", "<C-Down>", ":resize +2<Cr>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<Cr>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<Cr>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Owns Settings
+keymap("n", "<leader>w", ":w <Cr>", opts)
+keymap("n", "<leader>q", ":q! <Cr>", opts)
+
+
+
+-- INSERT --
+-- Press jk fast to enter
+keymap("i", "jk", "<ESC>", opts)
+keymap("v", "jk", "<ESC>", opts)
+
+
+-- VISUAL --
+-- Stay in ident mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1 <Cr>==", opts)
+keymap("v", "<A-k>", ":m .-2 <Cr>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+
+
+
---
+++ lua/user/options.lua
@@ -0,0 +1,36 @@
+-- :help options
+vim.opt.backup = false                          -- creates a backup file
+vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0		                    -- so that `` is visible in markdown files[
+vim.opt.fileencoding = "utf-8"			            -- the encoding written to a file
+vim.opt.hlsearch = true				                  -- highlight all matches on previus search pattern
+vim.opt.ignorecase = true			                  -- ignore case in search pattern
+vim.opt.mouse = "a"				                      -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10			  	                -- pop up menu height
+vim.opt.showmode = false			                  -- we don't need to see things like --INSERT -- anymore
+vim.opt.showtabline = 2				                  -- always show tabs
+vim.opt.smartcase = true			                  -- smart case
+vim.opt.smartindent = true			                -- make indenting smarter again
+vim.opt.splitbelow = true			                  -- force all horizontal splits to go below current window
+vim.opt.splitright = true			                  -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false			                  -- creates a swapfile
+-- vim.opt.termguicolors = true			            -- set term gui colors (most terminals support this)
+vim.opt.timeoutlen = 1000			                  -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undofile = true				                  -- enable persistent undo
+vim.opt.updatetime = 300			                  -- faster completion (4000ms default)
+vim.opt.expandtab = true			                  -- convert tabs to spaces
+vim.opt.shiftwidth = 2				                  -- the numbers of spaces inserted for each identation
+vim.opt.tabstop = 2				                      -- insert 2 spaces for a tab
+vim.opt.cursorline = true			                  -- highlight the current line
+vim.opt.number = true				                    -- set numbered lines
+vim.opt.relativenumber = false			            -- set relative numbered lines
+vim.opt.numberwidth = 4				                  -- set number column width to 2 {default 4}
+--vim.opt.signcolumn = "yes"			                -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false				                    -- display lines as one log line
+vim.opt.scrolloff = 8				                    -- I have to look at this in :help
+vim.opt.sidescrolloff = 8			
+vim.opt.guifont = "monospace:h17"		            -- the font used in graphical neovim applications
+
+-- vim.opt.shotmess:append "c"
